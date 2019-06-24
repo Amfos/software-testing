09315f3df821273b86174990208105ef431da6f3
Amfos <oleksiienko25@gmail.com>
2019-06-25 00:22:47 +0300

    New feature

---
+++ get_your_name.py
@@ -0,0 +1,15 @@
+class NewMe:
+
+   def __init__(self, name):
+       self.name = name
+       self.number = []
+
+   def average(self):
+       return sum(self.number) / len(self.number)
+
+
+avr_num = NewMe('Valeri')
+print avr_num.name
+avr_num.number.append(12)
+avr_num.number.append(55)
+print avr_num.average()
